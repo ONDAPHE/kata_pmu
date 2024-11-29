c0f90a0c99fadf1196f5b9570bcc0383c8f9090f
arthu <christianondaphe@gmail.com>
2024-11-29 19:20:06 +0100

    configuration du projet

---
+++ src/main/java/com/oca/kata_pmu/KataPmuApplication.java
@@ -0,0 +1,13 @@
+package com.oca.kata_pmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataPmuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataPmuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oca/kata_pmu/KataPmuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oca.kata_pmu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataPmuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
